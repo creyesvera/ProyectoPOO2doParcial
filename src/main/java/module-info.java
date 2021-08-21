1b8fe1e21b21a85f121084b27bfa2abec67f91b2
creyesvera <camdorey@espol.edu.ec>
2021-08-21 15:58:32 -0500

    opens y export del paquete controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module ec.edu.espol.proyectopoo2doparcial {
     requires javafx.fxml;
 
     opens ec.edu.espol.proyectopoo2doparcial to javafx.fxml;
+    opens ec.edu.espol.controller to javafx.fxml;
     exports ec.edu.espol.proyectopoo2doparcial;
+    exports ec.edu.espol.controller;
     requires java.mail;
     requires javafx.graphicsEmpty;
+    requires java.base;
 }
