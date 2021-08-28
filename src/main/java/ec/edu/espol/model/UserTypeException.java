96d815d1540a4cd4ca0c50e9621611677a0fef79
creyesvera <camdorey@espol.edu.ec>
2021-08-28 11:43:54 -0500

    creacion de la clase UserTypeException

---
+++ src/main/java/ec/edu/espol/model/UserTypeException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+/**
+ *
+ * @author camil
+ */
+public class UserTypeException extends Exception {
+
+    public UserTypeException(String message) {
+        super(message);
+    }
+    
+}
