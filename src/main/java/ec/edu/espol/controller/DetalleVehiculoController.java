cc1a5196ab22244380a67a9032e66575e1f09957
GingerGarcia <gngarcia@espol.edu.ec>
2021-09-05 09:47:17 -0500

    metodos ofertar

--- src/main/java/ec/edu/espol/controller/DetalleVehiculoController.java
+++ src/main/java/ec/edu/espol/controller/DetalleVehiculoController.java
@@ -5,15 +5,25 @@
  */
 package ec.edu.espol.controller;
 
+import ec.edu.espol.model.Usuario;
+import ec.edu.espol.model.Vehiculo;
+import static ec.edu.espol.util.Alarmas.alertaError;
+import static ec.edu.espol.util.Alarmas.alertaInformacion;
+import static ec.edu.espol.util.Util.esDouble;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -30,19 +40,76 @@ public class DetalleVehiculoController implements Initializable {
     private TextField txtPrecioOfer;
     @FXML
     private Button ofertar;
+    
+    private DetalleVehiculoController dtC;
+    private Usuario user;
+    private Vehiculo vehiculo;
+    private ArrayList<Usuario> usuarios;
+    
     @FXML
-    private VBox vbInfo;
-
+    private Label lblMarca;
+    @FXML
+    private Label lblAnio;
+    @FXML
+    private Label lblTdMotor;
+    
+    @FXML
+    private Label lblModelo;
+    @FXML
+    private Label lblRecorrido;
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        dtC = this;
+        usuarios = Usuario.readFile("usuarios.ser");
     }    
 
+    private void mostrarDatos(){
+        String ruta = getClass().getResource("/ec/edu/espol/imagenes_vehiculos/"+vehiculo.getImagen()).toExternalForm();
+        this.imgVehiculo.setImage(new Image(ruta));
+        lblMarca.setText(vehiculo.getMarca());
+        lblModelo.setText(vehiculo.getModelo());
+        lblAnio.setText(String.valueOf(vehiculo.getYear()));
+        lblTdMotor.setText(vehiculo.getTipo_motor());
+        lblRecorrido.setText(String.valueOf(vehiculo.getRecorrido()));
+        txtPrecioSug.setText(String.valueOf(vehiculo.getPrecio()));
+        buscarOferta();
+    }
     @FXML
     private void ofertar(ActionEvent event) {
+        if(validarCampos()){
+            user.comprar(vehiculo, "ofertas.ser", Double.parseDouble(txtPrecioOfer.getText()), usuarios); 
+            alertaInformacion("OFERTA REALIZADA","Se ha agregado una nueva oferta para el vehículo");
+            cerrarVentana();
+        }else
+            alertaError("Ha ocurrido un problema","Debe escribir un precio para ofertar");        
     }
     
+    private boolean validarCampos(){
+        return !(this.txtPrecioOfer.getText().isEmpty() || !esDouble(txtPrecioOfer.getText().trim()));
+    }
+    
+    public void recibirParametros(Usuario user, Vehiculo vehiculo){
+        this.user = user;
+        this.vehiculo = vehiculo;
+        mostrarDatos();
+    }
+    
+    private void buscarOferta(){
+        user.getOfertas().forEach(e->{
+            if(e.getVehiculo().equals(vehiculo))
+                this.txtPrecioOfer.setText(String.valueOf(e.getPrecio()));
+        });
+    }
+    
+    
+    private void cerrarVentana(){
+        Stage myStage = (Stage) this.lblAnio.getScene().getWindow();
+        myStage.close();
+    }
 }
