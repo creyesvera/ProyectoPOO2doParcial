d85822fd1f8749ecd33c2a12fdb55ae4b3192086
GingerGarcia <gngarcia@espol.edu.ec>
2021-09-05 04:14:35 -0500

    Modificacion en el modulo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ec.edu.espol.proyectopoo2doparcial {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens ec.edu.espol.model to javafx.base;
     opens ec.edu.espol.proyectopoo2doparcial to javafx.fxml;
     opens ec.edu.espol.controller to javafx.fxml;
     exports ec.edu.espol.proyectopoo2doparcial;
