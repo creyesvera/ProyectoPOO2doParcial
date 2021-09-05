3c4a5d6a9ff24177a66cbcf8d11ba7493a033419
jose012000 <josamoya@espol.edu.ec>
2021-09-05 06:34:27 -0500

    controlador de DetalleVehiculo

--- src/main/java/ec/edu/espol/controller/DetalleVehiculoController.java
+++ src/main/java/ec/edu/espol/controller/DetalleVehiculoController.java
@@ -3,11 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.espol.proyectopoo2doparcial;
+package ec.edu.espol.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -16,6 +22,17 @@ import javafx.fxml.Initializable;
  */
 public class DetalleVehiculoController implements Initializable {
 
+    @FXML
+    private ImageView imgVehiculo;
+    @FXML
+    private TextField txtPrecioSug;
+    @FXML
+    private TextField txtPrecioOfer;
+    @FXML
+    private Button ofertar;
+    @FXML
+    private VBox vbInfo;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +40,9 @@ public class DetalleVehiculoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void ofertar(ActionEvent event) {
+    }
     
 }
